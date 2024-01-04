a66c3c3bf7644a0e14a4f3d643d71dd3ad9bf20a
AoMiura <miura738@1oli.cn>
2024-01-04 16:28:35 +0800

    Test

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,48 @@
 import './globals.css'
-import Link from "next/link";
+import Test from "@/components/test";
 
 export default function Home() {
+
+
+
     return (
-        <main>
-            <header className="w-screen h-screen flex justify-center items-center">
-                <Link href={"/6/6"} className={"cursor-pointer w-[120px] h-[120px] flex justify-center items-center"}>
-                    <div className="rotate">⑥</div>
+        <main id={"main"} className={`overflow-y-visible overflow-x-hidden h-screen w-screen`}>
+            <header className="w-screen h-[calc(100vh)] overflow-hidden flex justify-center items-center relative">
+                    <div id={"button"}
+                         className={"cursor-pointer w-[120px] h-[120px] flex justify-center items-center"}>
+                        <div className="rotate">⑥</div>
 
-                </Link>
-
-                <div>
-                    <div className={`absolute z-10 bottom-16 right-10 w-20 rotate-180`}>
-                        <div className={`border border-t-black wbr`}></div>
-                    </div>
-                    <div className={`absolute z-10 bottom-10 right-16 h-52 rotate-180`}>
-                        <div className={`border border-l-black hbr`}></div>
                     </div>
 
-                    <div className={`border border-t-black wtl absolute z-10 top-16 left-10`}></div>
-                    <div className={`border border-l-black htl absolute z-10 top-10 left-16`}></div>
-                </div>
+                    <div>
+                        <div className={`absolute z-10 bottom-16 right-10 w-20 rotate-180`}>
+                            <div className={`border border-t-black wbr`}></div>
+                        </div>
+                        <div className={`absolute z-10 bottom-10 right-16 h-52 rotate-180`}>
+                            <div className={`border border-l-black hbr`}></div>
+                        </div>
 
-                <div className={`w-screen h-screen absolute top-0 left-0 bg-[url(https://loliapi.com/acg)] -z-50 bg-center bg-no-repeat bg-cover
-                after:w-screen after:h-screen after:absolute after:top-0 after:left-0 after:bg-white after:bg-opacity-75 after:-z-40`}>
+                        <div className={`absolute z-10 top-16 left-10 w-48`}>
+                            <div className={`border border-t-black wtl`}></div>
+                        </div>
+                        <div className={`absolute z-10 top-10 left-16 h-32`}>
+                            <div className={`border border-l-black htl`}></div>
+                        </div>
+                    </div>
 
-                </div>
+                    <div className={`w-full h-full absolute top-0 left-0 bg-[url(/static/download.jpg)] -z-50 bg-center bg-no-repeat bg-cover
+                after:w-full after:h-full after:absolute after:top-0 after:left-0 after:bg-white after:bg-opacity-75 after:-z-40`}>
+                    </div>
             </header>
 
-
+            <div className={`w-screen h-36 flex justify-center items-center`}>
+                666666666666666666666666666666666666666666666666666666666666
+            </div>
+            <div className={`w-screen h-screen flex justify-center items-center`}>
+                6666666666666666666666666666
+            </div>
+            <Test/>
+            {/*<script src={`/static/js/test.js`}></script>*/}
         </main>
 
     )
---
+++ components/test.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import {useEffect} from "react";
+
+const Test = ({href,children,...props}: { href: string,children?:string }) => {
+
+
+    useEffect(() => {
+        const MainView = document.getElementById("main");
+        const ButtonView = document.getElementById("button");
+
+        const handleScroll = (event) => {
+            if (MainView.scrollTop <= document.body.clientHeight && event.deltaY > 0) {
+                scroll();
+            }
+        }
+
+        function scroll() {
+            const MainView = document.getElementById("main");
+                MainView.scrollTo({
+                    top: document.body.clientHeight,
+                    behavior: "smooth"
+                });
+        }
+
+        MainView.addEventListener("wheel", handleScroll);
+        ButtonView.addEventListener("click", scroll);
+
+        return () => {
+            MainView.removeEventListener("wheel", handleScroll);
+            ButtonView.addEventListener("click", scroll);
+        };
+    }, []);
+    return
+
+}
+
+export default Test;
